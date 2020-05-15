4beedad61971a47b225278add79c87bd3bd9f920
Qays Ayad <work.worker55@gmail.com>
2020-05-15 22:20:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ function writeCards(cards,gift) {
   for (let i = 0; i < cards.length; i++) {
     console.log(`Thank you, ${cards[i]}, for the wonderful ${gift}!`);
   }
-
-  return gifts;
 }
 
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
