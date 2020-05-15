969fa615bc7cf915210463dc37429b01c95da71d
Qays Ayad <work.worker55@gmail.com>
2020-05-15 22:03:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solutions in this file
 const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGift(gift) {
-  console.log(`Wrapped ${gift} and added a bow!`);
+ 
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return gifts;
 }
-
-for (let i = 0; i < gifts.length; i++) {
-    wrapGift(gifts[i]);
-}
+ 
+wrapGifts(gifts);
