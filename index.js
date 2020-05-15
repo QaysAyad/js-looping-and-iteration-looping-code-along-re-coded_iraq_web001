89ac4ccae498bf5938770576334884dfe4eb7d78
Qays Ayad <work.worker55@gmail.com>
2020-05-15 22:24:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function writeCards(cards,gift) {
   const newArr = [];
   for (let i = 0; i < cards.length; i++) {
-    newArr.push(`Thank you, ${cards[i]}, for the wonderful ${gift}!`);
+    newArr.push(`Thank you, ${cards[i]}, for the wonderful ${gift} gift!`);
   }
 
   return newArr;
